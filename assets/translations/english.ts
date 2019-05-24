14ea875892960477aa750774a7ee4f8fd5a84b69
Razakhel <romain.milbert@gmail.com>
2019-05-24 23:37:22 +0200

    [Translation] Added English & French translation files

---
+++ assets/translations/english.ts
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../../src/RaZor/Interface/MainWindow.cpp" line="24"/>
+        <source>File</source>
+        <translation>File</translation>
+    </message>
+    <message>
+        <location filename="../../src/RaZor/Interface/MainWindow.cpp" line="29"/>
+        <source>Import...</source>
+        <translation>Import...</translation>
+    </message>
+    <message>
+        <location filename="../../src/RaZor/Interface/MainWindow.cpp" line="30"/>
+        <source>Import a file</source>
+        <translation>Import a file</translation>
+    </message>
+    <message>
+        <location filename="../../src/RaZor/Interface/MainWindow.cpp" line="37"/>
+        <source>Quit</source>
+        <translation>Quit</translation>
+    </message>
+    <message>
+        <location filename="../../src/RaZor/Interface/MainWindow.cpp" line="38"/>
+        <source>Quit RaZor</source>
+        <translation>Quit RaZor</translation>
+    </message>
+    <message>
+        <location filename="../../src/RaZor/Interface/MainWindow.cpp" line="47"/>
+        <source>Ready</source>
+        <translation>Ready</translation>
+    </message>
+</context>
+</TS>
