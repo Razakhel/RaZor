603418acedcbd9a48fa329ff6188f290e43f8969
Razakhel <romain.milbert@gmail.com>
2019-05-24 23:38:25 +0200

    [Main] Added a main file
    - The main instantiates an application, shows a splash screen with the RaZ logo on it, then displays the MainWindow

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include "RaZor/Interface/MainWindow.hpp"
+
+#include <QtWidgets/QtWidgets>
+
+int main(int argc, char* argv[]) {
+  QApplication app(argc, argv);
+  QCoreApplication::setOrganizationName("RaZor");
+  QCoreApplication::setApplicationName("RaZor");
+
+  QSplashScreen splashScreen(QPixmap(":/logo/256"));
+  splashScreen.show();
+
+  app.thread()->sleep(1);
+  app.processEvents();
+
+  MainWindow window;
+  window.show();
+
+  splashScreen.finish(&window);
+
+  return app.exec();
+}
