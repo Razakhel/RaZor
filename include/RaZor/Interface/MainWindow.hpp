e1e03d74c1fac6189abc4066ac3650bbeb9c8bb4
Razakhel <romain.milbert@gmail.com>
2019-05-24 23:35:06 +0200

    [Interface/MainWindow] Added menu & status bars
    - A 'File' menu is available, with 'Close' and a dummy 'Import' buttons

--- include/RaZor/Interface/MainWindow.hpp
+++ include/RaZor/Interface/MainWindow.hpp
@@ -3,8 +3,6 @@
 #ifndef RAZOR_MAINWINDOW_HPP
 #define RAZOR_MAINWINDOW_HPP
 
-#include <QtCore/QtCore>
-#include <QtGui/QtGui>
 #include <QtWidgets/QtWidgets>
 
 class MainWindow : public QMainWindow {
@@ -15,6 +13,17 @@ public:
 
 protected:
   void keyPressEvent(QKeyEvent* event) override;
+
+private slots:
+  void openFile() { qDebug() << "Opening file..."; }
+
+private:
+  QMenuBar* m_menuBar;
+  QMenu* m_fileMenu;
+  QAction* m_importAction;
+  QAction* m_quitAction;
+
+  QStatusBar* m_statusBar;
 };
 
 #endif // RAZOR_MAINWINDOW_HPP
--- src/RaZor/Interface/MainWindow.cpp
+++ src/RaZor/Interface/MainWindow.cpp
@@ -12,6 +12,41 @@ MainWindow::MainWindow() {
 
   setLocale(QLocale(QLocale::English, QLocale::UnitedStates));
 
+  // Menu bar
+  m_menuBar = new QMenuBar(this);
+  m_menuBar->setObjectName(QString::fromUtf8("menuBar"));
+  m_menuBar->setGeometry(QRect(0, 0, 800, 21));
+  setMenuBar(m_menuBar);
+
+  // Menu element 'File'
+  m_fileMenu = new QMenu(m_menuBar);
+  m_fileMenu->setObjectName(QString::fromUtf8("menuFile"));
+  m_fileMenu->setTitle(tr("File"));
+
+  // Menu actions
+  m_importAction = new QAction(m_fileMenu);
+  m_importAction->setObjectName(QString::fromUtf8("actionImport"));
+  m_importAction->setText(tr("Import..."));
+  m_importAction->setToolTip(tr("Import a file"));
+  m_importAction->setShortcut(QKeySequence::Open);
+  connect(m_importAction, &QAction::triggered, this, &MainWindow::openFile);
+  m_fileMenu->addAction(m_importAction);
+
+  m_quitAction = new QAction(m_fileMenu);
+  m_quitAction->setObjectName(QString::fromUtf8("actionQuit"));
+  m_quitAction->setText(tr("Quit"));
+  m_quitAction->setToolTip(tr("Quit RaZor"));
+  connect(m_quitAction, &QAction::triggered, this, &QMainWindow::close);
+  m_fileMenu->addAction(m_quitAction);
+
+  m_menuBar->addAction(m_fileMenu->menuAction());
+
+  // Status bar
+  m_statusBar = new QStatusBar(this);
+  m_statusBar->setObjectName(QString::fromUtf8("statusBar"));
+  m_statusBar->showMessage(tr("Ready"));
+  setStatusBar(m_statusBar);
+
   QMetaObject::connectSlotsByName(this);
 }
 
